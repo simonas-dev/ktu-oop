cff921a4dbc2415f06f9ccf55b8d6983d8ce9ac9
LUKAS <lukas.semaska@gmail.com>
2018-12-14 09:20:50 +0200

    fixes

--- Shared/SpaceInvaders.Shared.Repository/PlayerDbContext.cs
+++ Shared/SpaceInvaders.Shared.Repository/PlayerDbContext.cs
@@ -10,7 +10,8 @@ namespace SpaceInvaders.Shared.Repository
         {
             new Player() { Name = "Hank" },
             new Player() { Name = "John" },
-            new Player() { Name = "Paul" }
+            new Player() { Name = "Paul" },
+            new Player() { Name = "Justtest" }
         };
 
 
